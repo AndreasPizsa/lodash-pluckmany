58272b0777e029c4a86e2571ad8846ee30f8d7a2
Andreas Pizsa <andreas@butterseite.com>
2015-11-03 14:03:46 +0000

    first commit

---
+++ pluckmany.js
@@ -0,0 +1,15 @@
+_ = require('lodash');
+
+/* http://stackoverflow.com/a/27514972/199263 */
+
+_.mixin({
+    pluckMany: function() {
+        var array = arguments[0],
+            propertiesToPluck = _.rest(arguments, 1);
+
+        return _.map(array, function(item) {
+            return _.partial(_.pick, item).apply(null, propertiesToPluck);
+        });
+    }
+});
+
